e1645d182fe9793e71d762d196adede71d57088a
Edgar-Moises-L <moisesleyva202@gmail.com>
2026-06-09 11:16:11 -0700

    feat: implementacion de Mappstruct para la interfas de mapper de product

--- src/main/java/punto_de_venta/product/infrastructure/mapper/ProductMapper.java
+++ src/main/java/punto_de_venta/product/infrastructure/mapper/ProductMapper.java
@@ -1,7 +1,20 @@
 package punto_de_venta.product.infrastructure.mapper;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import punto_de_venta.product.domain.Product;
+import punto_de_venta.product.infrastructure.dto.ProductRequest;
+import punto_de_venta.product.infrastructure.dto.ProductResponse;
+import punto_de_venta.product.infrastructure.entity.ProductEntity;
 
-@Component
-public class ProductMapper {
+@Mapper
+public interface ProductMapper {
+    @Mapping(target = "id" , ignore = true)
+    public Product productRequestToProduct(ProductRequest productRequest);
+
+    public ProductEntity productToProductEntity(Product product);
+
+    public Product productEntityToProduct(ProductEntity productEntity);
+
+    public ProductResponse productToProductResponse(Product product);
 }
