cfe7452e7624a83e1f5f1dae65e4728c5a5b3da0
Edgar-Moises-L <moisesleyva202@gmail.com>
2026-06-09 12:48:12 -0700

    feat: implementacion de la clases controller

--- src/main/java/punto_de_venta/product/infrastructure/controller/ProductControllerImpl.java
+++ src/main/java/punto_de_venta/product/infrastructure/controller/ProductControllerImpl.java
@@ -1,8 +1,11 @@
 package punto_de_venta.product.infrastructure.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import punto_de_venta.product.domain.Product;
 import punto_de_venta.product.domain.ProductService;
 import punto_de_venta.product.infrastructure.dto.ProductRequest;
 import punto_de_venta.product.infrastructure.dto.ProductResponse;
@@ -11,33 +14,52 @@ import punto_de_venta.product.infrastructure.mapper.ProductMapper;
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
+@RequestMapping("/api/product")
 public class ProductControllerImpl implements ProductController {
     private final ProductService service;
     private final ProductMapper mapper;
 
     @Override
-    public ResponseEntity<ProductResponse> create(ProductRequest productRequest) {
-        return null;
+    @PostMapping
+    public ResponseEntity<ProductResponse> create(@Valid @RequestBody ProductRequest productRequest) {
+        Product product = mapper.productRequestToProduct(productRequest);
+        Product productReturn = service.createProduct(product);
+        ProductResponse productResponse = mapper.productToProductResponse(productReturn);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productResponse);
     }
 
     @Override
+    @GetMapping
     public ResponseEntity<List<ProductResponse>> getAll() {
-        return null;
+        List<ProductResponse> productResponses = service.getAllProducts()
+                .stream()
+                .map(mapper::productToProductResponse)
+                .toList();
+        return ResponseEntity.ok(productResponses);
     }
 
     @Override
-    public ResponseEntity<ProductResponse> getById(Long id) {
-        return null;
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getById(@PathVariable Long id) {
+        Product product = service.getProductByid(id);
+        ProductResponse productResponse = mapper.productToProductResponse(product);
+        return ResponseEntity.ok(productResponse);
     }
 
     @Override
-    public ResponseEntity<ProductResponse> update(Long id, ProductRequest productRequest) {
-        return null;
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> update(@Valid @PathVariable @RequestBody Long id, ProductRequest productRequest) {
+        Product product = mapper.productRequestToProduct(productRequest);
+        Product productUpdate = service.updateProduct(id, product);
+        ProductResponse productResponse = mapper.productToProductResponse(productUpdate);
+        return ResponseEntity.ok(productResponse);
     }
 
     @Override
-    public ResponseEntity<Void> delete(Long id) {
-        return null;
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.deleteProduct(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
